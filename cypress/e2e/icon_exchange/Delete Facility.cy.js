6c5d81bd12230b3f1bbea2e672a0f991276434bc
Ansab-Sajjad <ansab@troontechnologies.com>
2023-01-24 17:33:24 +0500

    initial commit

---
+++ cypress/e2e/icon_exchange/Delete Facility.cy.js
@@ -0,0 +1,10 @@
+function Icon_Exchange_Login() {
+
+it('Click on the Facility.', () => {
+    cy.get('.submenu.active > .relative > .cursor-pointer').click()
+});
+
+it('', () => {
+    cy.get(':nth-child(1) > :nth-child(7) > .table-dropdown > #dropdown-basic > .icon-Vertical-Dots').click()
+});
+}
