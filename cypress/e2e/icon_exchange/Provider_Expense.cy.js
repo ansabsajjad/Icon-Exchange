732a0c95344b9c284e0537c6caae55a9bc053695
Ansab-Sajjad <ansab@troontechnologies.com>
2023-04-04 10:45:09 +0500

    Add more

--- cypress/e2e/icon_exchange/Provider_Expense.cy.js
+++ cypress/e2e/icon_exchange/Provider_Expense.cy.js
@@ -77,14 +77,14 @@ function Provider_Expense(){
 
 
     it('EX- Enter Expense Start Date. ', () => {
-        cy.get('[name="expenses.0.expenseSpentStartDate"]').click({force:true}).type("04/12/2023 - 04/27/2023")
+        cy.get('[name="expenses.0.expenseSpentStartDate"]').click({force:true}).type("04/19/2023 - 04/21/2023")
     });
 
 
     it('EX- Enter Description.', () => {
         cy.get('[name="expenses.0.description"]').type("This is description.")
     });
-    it('', () => {
+    it('Ex- Upload Attahment.', () => {
         cy.get('.btn-browse').click({force:true})
         const filepath11 = "a11.jpg"
         cy.get('.btn-browse').attachFile(filepath11,{subjectType: 'drag-n-drop'})
@@ -92,10 +92,50 @@ function Provider_Expense(){
     });
 
 
+
+
+
+    it('EX- Click on Add more.', () => {
+        cy.get('.addmore').click().wait(3000)
+    });
+
+
+    it('EX-Slect Expense Type. ', () => {
+        cy.get('[id="react-select-16-input"]').click({force:true}).wait(2000)
+        cy.get('#react-select-16-option-0').click({force: true})   /////===============Array Index 0
+        cy.wait(2000)
+    });
+
+
+    it('EX- Enter total amount. ', () => {
+        cy.get('[data-testid="expenses.1.totalAmount"]').click().type("56")
+    });
+
+
+    it('EX- Enter Expense Spent date. ', () => {
+        cy.get('[name="expenses.1.expenseSpentStartDate"]').click().type("05/22/2023 - 05/25/2023")
+    });
+
+
+    it('EX- Enter Description.', () => {
+        cy.get('[name="expenses.1.description"]').type("This is description.")
+    });
+    it('Ex- Upload Attahment.', () => {
+        cy.get(':nth-child(3) > .row > .col-md-12 > .form-group > .dropzone > .global-file-style > .d-flex > :nth-child(2) > .btn-browse').click({force:true})
+        const filepath11 = "a11.jpg"
+        cy.get(':nth-child(3) > .row > .col-md-12 > .form-group > .dropzone > .global-file-style > .d-flex > :nth-child(2) > .btn-browse').attachFile(filepath11,{subjectType: 'drag-n-drop'})
+        cy.wait(5000)
+    });
+
     it('EX- Click on Create button. ', () => {
         // cy.wait(30000)
         cy.get('[type="submit"]').click({force:true})
     });
+    
+    it('EX- ', () => {
+        
+    });
+
 
 
     it('EX- ', () => {
@@ -103,14 +143,65 @@ function Provider_Expense(){
     });
 
 
+
     it('EX- ', () => {
         
     });
 
 
+
     it('EX- ', () => {
         
     });
+
+
+
+    it('EX- ', () => {
+        
+    });
+
+
+
+    it('EX- ', () => {
+        
+    });
+
+
+
+    it('EX- ', () => {
+        
+    });
+
+
+
+    it('EX- ', () => {
+        
+    });
+
+
+
+    it('EX- ', () => {
+        
+    });
+
+
+
+    it('EX- ', () => {
+        
+    });
+
+
+
+    it('EX- ', () => {
+        
+    });
+
+
+
+    it('EX- ', () => {
+        
+    });
+
 
 
     it('EX- ', () => {
