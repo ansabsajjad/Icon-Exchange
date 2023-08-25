349e703d362b7f396edccec6035d518d8d0630a0
Ansab-Sajjad <ansab@troontechnologies.com>
2023-08-25 17:01:23 +0500

    code update

--- cypress/e2e/icon_exchange/Bulk_Facility_Assignment.cy.js
+++ cypress/e2e/icon_exchange/Bulk_Facility_Assignment.cy.js
@@ -11,7 +11,7 @@ function Bulk_Facility_Assignment() {
 
 
 // Array of indices to loop through
-const startIndex = 1;
+const startIndex = 39;
 const endIndex = 100;
 const emailPattern = 'ansab+BP';
 const emailDomain = '@troontechnologies.com';
@@ -25,15 +25,16 @@ for (let index = startIndex; index <= endIndex; index++) {
 
     it('Cycle ' + (index) + ' Click on 3 dot button.', () => {
        
+        cy.get('[data-testid="submenu-providers-listing"]').click({force:true}).wait(5000)
   
             
         cy.wait(2000);
         cy.get(`:nth-child(${index}) > .stickycolumn > .table-dropdown > #dropdown-basic > .icon-Vertical-Dots`)
           .click({ force: true })
-          .wait(2000);
+
     
           
-                cy.get('[data-testid="view-assigned-facilities"]').wait(1000).click();
+             cy.get('[data-testid="view-assigned-facilities"]').wait(1000).click();
             cy.wait(4000)
             cy.get('[data-testid="assign-facility-btn"]').click()
             cy.wait(4000)
@@ -42,89 +43,88 @@ for (let index = startIndex; index <= endIndex; index++) {
 
        // ====> Facility Name
     cy.get('#facility > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click()
-    cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+    cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(100)
           
        // ====> facility contracted by
     cy.get('#contractedBy > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click()
-    cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+    cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(1000)
     
        
          
         // ====> provider working for
         cy.get('#workingFor > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true})
-        cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+        cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(1000)
        
          
         // ====> Area of expertise
         cy.get('#areaOfExpertise > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(2000)
-        cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+        cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(1000)
        
           
         // ====> paper work submitted to
         cy.get('#paperWorkSubmittedToFacility > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(2000)
-        cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+        cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(1000)
 
           
         // ====> faicility approval
-        cy.get('#facilityApproval > .custom-select__control > .custom-select__value-container').click({force:true}).wait(2000)
-        cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+        cy.get('#facilityApproval > .custom-select__control > .custom-select__value-container').click({force:true}).wait(1000)
+        cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(1000)
 
           
         // ====> enrollment status
-        cy.get('#payorEnrollmentStatus > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(2000)
-        cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+        cy.get('#payorEnrollmentStatus > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(1000)
+        cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(1000)
        
           
         // ====> currently active
-        cy.get('#currentlyActive > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(2000)
-        cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+        cy.get('#currentlyActive > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(1000)
+        cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(1000)
        
         //======> Effective Start & End date
-        cy.get('[id="effectiveStartDate"]').type("08/22/2023").type('{enter}').wait(1000)
-        cy.get('[id="effectiveEndDate"]').type("08/30/2023").type('{enter}').wait(1000)
+        cy.get('[id="effectiveStartDate"]').type("08/22/2023").type('{enter}').wait(100)
+        cy.get('[id="effectiveEndDate"]').type("08/30/2023").type('{enter}').wait(100)
 
 
          // ====> Malpractice insurance
-         cy.get('#malpracticeInsurance > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(2000)
-         cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+         cy.get('#malpracticeInsurance > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(1000)
+         cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(1000)
         
 
          // ====> Recuriter
-         cy.get('#recruiter > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(2000)
-         cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+         cy.get('#recruiter > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(1000)
+         cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(1000)
         
 
 
          // ====> Account manager
-         cy.get('#accountManager > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(2000)
-         cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+         cy.get('#accountManager > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(1000)
+         cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(1000)
         
          
          // ====> Credentialling coordinator
-         cy.get('#credentialingCoordinator > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(2000)
-         cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+         cy.get('#credentialingCoordinator > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(1000)
+         cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(1000)
         
 
 
 
 
          // ====> Client
-         cy.get('#client > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(2000)
-         cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+         cy.get('#client > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(1000)
+         cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(1000)
         
 
 
          
          // ====> job type
-         cy.get('#jobType > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(2000)
-         cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(2000)
+         cy.get('#jobType > .custom-select__control > .custom-select__value-container > .custom-select__input-container').click({force:true}).wait(1000)
+         cy.get('.custom-select__option[tabindex="-1"]:first').click().wait(1000)
         
 
 
 
          cy.get('[data-testid="createbtn"]').click({force:true}).wait(5000)
 
-        cy.get('[data-testid="submenu-providers-listing"]').click({force:true}).wait(5000)
 
 
 
